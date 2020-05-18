7cebe165fec3a17534529974983df4a795f54bd1
Manish <manish@Manishs-MacBook-Air.local>
2020-05-18 12:08:21 +0530

    initial comment

---
+++ src/test/java/webDriverMaven/LoginTest.java
@@ -0,0 +1,39 @@
+package webDriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver; 
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		System.setProperty("webdriver.gecko.driver", "/Users/manish/Desktop/Manish/Workspace/Selenium/geckodriver");
+		driver = new FirefoxDriver();
+		
+	}
+	
+	@Test	
+	public void doLogin() {
+		
+		driver.get("https://trial.rivers.im/");
+		
+		
+		
+		driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/form/div[1]/input")).sendKeys("+91");
+		driver.findElement(By.id("phoneNumber")).sendKeys("8527799401");
+	
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
